0abf65a7bfd8f68e7c699eeaf3afb2b74d1f7544
Lee-Null <nullist.null@gmail.com>
2018-12-07 00:11:03 +0900

    dummy claim take

---
+++ Dummy/take.sql
@@ -0,0 +1,3 @@
+INSERT INTO Take(claim_id, employee_id, finish_time) VALUES(1, 1234, NOW());
+INSERT INTO Take(claim_id, employee_id, finish_time) VALUES(1, 4312, NOW());
+INSERT INTO Take(claim_id, employee_id, finish_time) VALUES(1, 3412, NOW());
