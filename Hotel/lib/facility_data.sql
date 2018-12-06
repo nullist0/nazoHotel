6a0d8d51157b58edf04542741c5c320c35b71074
davidkmw0810 <davidkmw0810@gmail.com>
2018-12-07 00:46:00 +0900

    더미데이터만 남김

--- Hotel/lib/facility_data.sql
+++ Hotel/lib/facility_data.sql
@@ -1,38 +1,3 @@
-CREATE TABLE 'Facility' 
-(
-	'facility_id' int AUTO_INCREMENT,
-	'floor' int,
-	'type' varchar(255),
-	'main_staff_id' int,
-	'sub_staff_id' int,
-	-- PRIMARY KEY('facility_id'),
-	-- FOREIGN KEY('main_staff_id') REFERENCES 'Employee'('employee_id'),
-	-- FOREIGN KEY('sub_staff_id') REFERENCES 'Employee'('employee_id')
-);
-
-CREATE TABLE 'Use' 
-(
-	'facility_id' int,
-	'room_id' int,
-	'date' date,
-	'start_time' datetime,
-	'end_time' datetime,
-	-- PRIMARY KEY('facility_id', 'room_id', 'date'),
-	-- FOREIGN KEY('room_id') REFERENCES 'Room'('room_id'),
-	-- FOREIGN KEY('facility_id') REFERENCES 'Facility'('facility_id')
-);
-
-CREATE TABLE 'Fix' 
-(
-	'fix_id' int,
-	'facility_id' int,
-	'employee_id' int,
-	'fixed_time' datetime,
-	-- PRIMARY KEY('fix_id'),
-	-- FOREIGN KEY('employee_id') REFERENCES 'Employee'('employee_id'),
-	-- FOREIGN KEY('facility_id') REFERENCES 'Facility'('facility_id')
-);
-
 insert into 'Facility' values(2000001,3,'fire extinguisher',3000001,3000002);
 insert into 'Facility' values(2000002,2,'emergency light',3000003,3000004);
 insert into 'Facility' values(2000003,1,'light',3000005,3000006);
