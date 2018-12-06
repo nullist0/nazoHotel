6e3948a5b7bf5d157192001056938261770311ba
davidkmw0810 <davidkmw0810@gmail.com>
2018-12-06 18:48:49 +0900

    createBook

--- Hotel/routes/book.js
+++ Hotel/routes/book.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var book = require('../lib/book')
 
 //예약 세부
 router.get('/', function(req, res, next) {
@@ -8,7 +9,8 @@ router.get('/', function(req, res, next) {
 
 //예약생성
 router.post('/', function(req, res, next) {
-  
+    book.createBook(req.body, function(results){
+    });
 });
 
 //예약정보 검색
@@ -18,7 +20,7 @@ router.get('/search', function(req, res, next) {
 
 //예약정보 검색처리
 router.post('/search', function(req, res, next) {
-  
+
 });
 
 //예약정보 조회
@@ -28,7 +30,9 @@ router.get('/:id', function(req, res, next) {
 
 //예약정보 삭제
 router.delete('/:id', function(req, res, next) {
-  
+    book.delete(req.body, function(results){
+        res.redirect('/home');
+    });
 });
 
 module.exports = router;
