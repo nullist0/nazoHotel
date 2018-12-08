7fbb300e59327845d7c7949bc29b22399e70e36f
Lee-Null <nullist.null@gmail.com>
2018-12-08 21:02:21 +0900

    Modify sqls

--- Dummy/employeee.sql
+++ Dummy/employeee.sql
@@ -1,3 +1,3 @@
-insert into employee VALUES(1,"홍길동","경영","안산시 한대앞로","gildonH@naver.com","male","01011112222","2013-09-23",2300000,101);
-insert into employee VALUES(2,"이순신","응대","용인시 보정로","GuaBook2@naver.com","female","01044445555","2014-11-03",1800000,102);
-insert into employee VALUES(3,"김두한","기획","수원시 팔달로","pist33@naver.com","male","01077779999","2012-01-17",2500000,103);
+insert into Employee VALUES(1,"홍길동","경영","안산시 한대앞로","gildonH@naver.com","male","01011112222","2013-09-23",2300000,101);
+insert into Employee VALUES(2,"이순신","응대","용인시 보정로","GuaBook2@naver.com","female","01044445555","2014-11-03",1800000,102);
+insert into Employee VALUES(3,"김두한","기획","수원시 팔달로","pist33@naver.com","male","01077779999","2012-01-17",2500000,103);
--- Dummy/vacation.sql
+++ Dummy/vacation.sql
@@ -1,1 +1,1 @@
-insert into vacation VALUES(1, "2018-12-7", "2018-12-15", "병가");
+insert into Vacation VALUES(1, "2018-12-7", "2018-12-15", "병가");
