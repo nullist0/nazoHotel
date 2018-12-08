083b8bdfe09a07445b2bd5482e563a8a2defab25
tmdgus2568 <tmdgus2568@naver.com>
2018-12-08 21:26:29 +0900

    book

--- Dummy/Option_Kind.sql
+++ Dummy/Option_Kind.sql
@@ -1,4 +1,4 @@
-INSERT INTO `Option_Kind`(`option_name`, `option_price_per_num`) VALUES ("흡연 가능",25);
-INSERT INTO `Option_Kind`(`option_name`, `option_price_per_num`) VALUES ("오션 뷰",30);
-INSERT INTO `Option_Kind`(`option_name`, `option_price_per_num`) VALUES ("조식 뷔페",20);
-INSERT INTO `Option_Kind`(`option_name`, `option_price_per_num`) VALUES ("석식 뷔페",25);
+INSERT INTO `Option_Kind`(`option_name`, `option_price_per_num`) VALUES ("흡연 가능",25000);
+INSERT INTO `Option_Kind`(`option_name`, `option_price_per_num`) VALUES ("오션 뷰",30000);
+INSERT INTO `Option_Kind`(`option_name`, `option_price_per_num`) VALUES ("조식 뷔페",20000);
+INSERT INTO `Option_Kind`(`option_name`, `option_price_per_num`) VALUES ("석식 뷔페",25000);
